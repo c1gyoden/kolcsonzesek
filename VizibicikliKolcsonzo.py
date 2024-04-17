833e16e205e89ae5dc580fa9ba81a14014c4c58a
c1gyoden <c1gyoden@vasvari.org>
2024-04-17 08:26:53 +0200

    2. feladat

---
+++ VizibicikliKolcsonzo.py
@@ -0,0 +1,8 @@
+class Kolcsonzes:
+    def __init__(self, nev, jazon, eora, eperc, vora, vperc):
+        self.nev = nev
+        self.jazon = jazon
+        self.eora = eora
+        self.eperc = eperc
+        self.vora = vora
+        self.vperc = vperc
