9d7db26afa4e3fde236f933b541c0b2aac2731ff
c1gyoden <c1gyoden@vasvari.org>
2024-04-17 08:32:59 +0200

    4. feladat

--- VizibicikliKolcsonzo.py
+++ VizibicikliKolcsonzo.py
@@ -9,3 +9,13 @@ class Kolcsonzes:
 
     def __str__(self):
         return f'Név: {self.nev}\nAzonosító: {self.jazon}\n'
+    
+lista = []
+
+fajl = open('kolcsonzesek.txt', 'rt', encoding='utf-8')
+fajl.readline()
+
+for sor in fajl:
+    sor = sor.strip().split(';')
+    lista.append(Kolcsonzes(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5]))
+
